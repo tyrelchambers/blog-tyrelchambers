249babfe770a7af0b5db9f2ae859308460873851
Tyrel Chambers <tychambers3@gmail.com>
2024-12-21 13:39:37 -0500

    init

---
+++ src/config.ts
@@ -0,0 +1,59 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  author: "Sat Naing",
+  profile: "https://satnaing.dev/",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "Tyrel Chambers",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  editPost: {
+    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
+    text: "Suggest Changes",
+    appendFilePath: true,
+  },
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/tyrelchambers",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Instagram",
+    href: "https://instagram.com/imtyrelchambers",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: true,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/tyrel-chambers-8ab581214/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "X",
+    href: "https://x.com/imtyrelchambers",
+    linkTitle: `${SITE.title} on X`,
+    active: true,
+  },
+];
---
+++ src/pages/about.md
@@ -0,0 +1,6 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+I'm Tyrel Chambers and I'm a Software Enginner, Customer Success hybrid. I love building projects I find exciting regardless of their utility. I love taking photographs and going for drives in the snow. There's a lot to me that makes me, me and most of my time is trying to figure out what that is, exactly. I hope you enjoy the writings I have here and that you find something that resonates with you. If you'd like to see some of my work, head over my [portfolio](https://tyrelchambers.com) to see some of my projects there.
